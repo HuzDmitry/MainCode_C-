f377d3ede7be73d1689fe475cdd29d87f24a5d4e
HuzDmitry <guzdima@tut.by>
2023-04-07 23:24:36 +0300

    implemetation DZ 2

--- C#/DZ/Program.cs
+++ C#/DZ/Program.cs
@@ -1,11 +1,27 @@
-﻿Console.WriteLine("Введите первое число для сравнения: ");
-int x=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число для сравнения: ");
-int y=Convert.ToInt32(Console.ReadLine());
-if (x>y)
+﻿// задача 2
+// Console.WriteLine("Введите первое число для сравнения: ");
+// int x=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число для сравнения: ");
+// int y=Convert.ToInt32(Console.ReadLine());
+// if (x>y)
+// {
+//     Console.WriteLine("max: "+x);
+// }else
+// {
+//     Console.WriteLine("max: "+y);
+// }
+
+// задача 4
+Console.WriteLine("Введите первое число для сравнения: ");
+int x=0;
+for (int i = 0; i < 3; i++)
 {
-    Console.WriteLine("max: "+x);
-}else
-{
-    Console.WriteLine("max: "+y);
+   int temp=Convert.ToInt32(Console.ReadLine());
+    if (temp>x)
+    {
+        x=temp;
+    }
+    Console.WriteLine("Введите еще число для сравнения: ");
+
 }
+Console.WriteLine("max= "+x);
