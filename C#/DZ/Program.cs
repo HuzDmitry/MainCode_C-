08c8611186b2ffb6d7b4f4a9748bf7abc46e3153
HuzDmitry <guzdima@tut.by>
2023-04-07 22:58:03 +0300

    implemetation DZ 1

---
+++ C#/DZ/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число для сравнения: ");
+int x=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число для сравнения: ");
+int y=Convert.ToInt32(Console.ReadLine());
+if (x>y)
+{
+    Console.WriteLine("max: "+x);
+}else
+{
+    Console.WriteLine("max: "+y);
+}
