97398dcca029222ca43d67211f874e7f9696cfec
HuzDmitry <guzdima@tut.by>
2023-04-07 23:36:46 +0300

    implemetation DZ 3

--- C#/DZ/Program.cs
+++ C#/DZ/Program.cs
@@ -12,16 +12,27 @@
 // }
 
 // задача 4
-Console.WriteLine("Введите первое число для сравнения: ");
-int x=0;
-for (int i = 0; i < 3; i++)
-{
-   int temp=Convert.ToInt32(Console.ReadLine());
-    if (temp>x)
-    {
-        x=temp;
-    }
-    Console.WriteLine("Введите еще число для сравнения: ");
+// Console.WriteLine("Введите первое число для сравнения: ");
+// int x=0;
+// for (int i = 0; i < 3; i++)
+// {
+//    int temp=Convert.ToInt32(Console.ReadLine());
+//     if (temp>x)
+//     {
+//         x=temp;
+//     }
+//     Console.WriteLine("Введите еще число для сравнения: ");
 
+// }
+// Console.WriteLine("max= "+x);
+
+//задача 6
+Console.WriteLine("Введите число для анализа: ");
+int x=Convert.ToInt32(Console.ReadLine());
+if (x%2==0)
+{
+    Console.WriteLine(x+" четное");
+}else
+{
+    Console.WriteLine(x+" не четное");
 }
-Console.WriteLine("max= "+x);
