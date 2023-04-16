3c991b601a0ac2bb5596f0747ca626c905709e05
HuzDmitry <guzdima@tut.by>
2023-04-16 22:28:17 +0300

    feat: DZ №10; 13; 15

--- C#/DZ/Program.cs
+++ C#/DZ/Program.cs
@@ -37,14 +37,72 @@
 //     Console.WriteLine(x+" не четное");
 // }
 // задача 8
-Console.WriteLine("Введите число для анализа: ");
-int x=Convert.ToInt32(Console.ReadLine());
-int temp=1;
-while (temp<=x)
+// Console.WriteLine("Введите число для анализа: ");
+// int x=Convert.ToInt32(Console.ReadLine());
+// int temp=1;
+// while (temp<=x)
+// {
+//     if (temp%2==0)
+//     {
+//         Console.Write(temp+", ");
+//     }
+//     temp++;
+// }
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.WriteLine("Введите трехзначное число для анализа: ");
+// int num=Convert.ToInt32(Console.ReadLine());
+// int x1=num%100/10;
+// Console.WriteLine(x1);
+
+// адача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.WriteLine("Введите число для анализа: ");
+// int num=Convert.ToInt32(Console.ReadLine());
+
+// if (num<100)
+// {
+//     Console.WriteLine("В этом числе нет третьего числа");
+// }
+// else 
+// {
+//     Console.WriteLine(GetInteger(num));
+// }
+// int GetInteger(int numb)
+// {
+//     while(numb>999)
+//     {
+//         numb/=10;
+//     }
+//     return numb%10;
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число дня недели: ");
+int num=Convert.ToInt32(Console.ReadLine());
+if (num<0 || num>8)
 {
-    if (temp%2==0)
-    {
-        Console.Write(temp+", ");
-    }
-    temp++;
+    Console.WriteLine("в недели всего 7 дней.");
+}else if(num==6 || num==7)
+{
+    Console.WriteLine("это выходной день.");
+}else
+{
+    Console.WriteLine("это рабочий день.");
 }
