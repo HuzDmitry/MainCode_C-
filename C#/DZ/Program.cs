4a4e6ed895c55b83e38592770c6996be641db69c
HuzDmitry <guzdima@tut.by>
2023-05-20 00:01:39 +0300

    DZ урок 8 задача 54

--- C#/DZ/Program.cs
+++ C#/DZ/Program.cs
@@ -576,7 +576,59 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int [,] CreateArray()
+// int [,] CreateArray()
+// {
+//     Console.WriteLine("Введите колличество строк в массиве:");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите количество столбцов в массиве:");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     int [,] array =new  int [rows,columns];
+//     return array;
+// }
+// int[,] GetRandomArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//       for (int j = 0; j < array.GetLength(1); j++)
+//        {
+//             array[i,j] =new Random().Next(10);
+//           Console.Write(array[i,j]+  " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     return array;
+// }
+
+// int[,] result=GetRandomArray(CreateArray());
+// double[]arifmet=new double[result.GetLength(1)];
+
+// for (int i = 0; i < result.GetLength(0); i++)
+//     {
+//       for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             arifmet[j] +=result[i,j];
+//         }
+//     }
+// Console.WriteLine();
+// for (int i = 0; i < arifmet.Length; i++)
+// {
+//     arifmet[i]/=result.GetLength(1);
+//     Console.Write($"{Math.Round(arifmet[i],1)}; ");
+// }
+//-----------------------------------------------------------------------------
+// Урок 8. Двумерные массивы. Продолжение
+
+// Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+int [,] CreateArray()//создание массива
 {
     Console.WriteLine("Введите колличество строк в массиве:");
     int rows = Convert.ToInt32(Console.ReadLine());
@@ -585,33 +637,56 @@ int [,] CreateArray()
     int [,] array =new  int [rows,columns];
     return array;
 }
-int[,] GetRandomArray(int[,] array)
+int[,] GetRandomArray(int[,] array, int length)// заполнение массива случайными числами
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
       for (int j = 0; j < array.GetLength(1); j++)
        {
-            array[i,j] =new Random().Next(10);
-          Console.Write(array[i,j]+  " ");
+            array[i,j] =new Random().Next(length);
         }
-        Console.WriteLine();
     }
     return array;
 }
-
-int[,] result=GetRandomArray(CreateArray());
-float[]arifmet=new float[result.GetLength(1)];
-
-for (int i = 0; i < result.GetLength(0); i++)
-    {
-      for (int j = 0; j < result.GetLength(1); j++)
-        {
-            arifmet[j] +=result[i,j];
-        }
-    }
-Console.WriteLine();
-for (int i = 0; i < arifmet.Length; i++)
+void PrintArray(int[,]result)// вывод массива в консоль
 {
-    arifmet[i]/=result.GetLength(1);
-    Console.Write($"{Math.Round(arifmet[i],1)}; ");
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            Console.Write(result[i,j]);
+        }
+
+        Console.WriteLine();
+    }
 }
+void SortArray(int[,] array)
+{
+    
+    for(int i=0; i<array.GetLength(0);i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int count=array[i,j];
+            int k=j+1;
+            int temp=0;
+            while(k<array.GetLength(1))
+            {
+                if (count<array[i,k])
+                {
+                    temp=count;
+                    count=array[i,k];
+                    array[i,k]=temp;
+                }
+                k++;
+            } 
+            array[i,j]=count;
+        }
+    } 
+}
+
+int [,]result=GetRandomArray(CreateArray(), 10);
+PrintArray(result);
+SortArray(result);
+Console.WriteLine();
+PrintArray(result);
---
+++ C#/Seminar_4/Program.cs
@@ -0,0 +1,147 @@
+﻿//Задача 53: Задайте двумерный массив. Напишите программу,
+ //которая поменяет местами первую и последнюю строку массива.
+
+// int [,] CreateArray()
+// {
+//     Console.WriteLine("Введите колличество строк в массиве:");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите количество столбцов в массиве:");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     int [,] array =new  int [rows,columns];
+//     return array;
+// }
+// int[,] GetRandomArray(int[,] array, int length)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//       for (int j = 0; j < array.GetLength(1); j++)
+//        {
+//             array[i,j] =new Random().Next(length);
+//         }
+//     }
+//     return array;
+// }
+
+
+// void PrintArray(int[,]result)
+// {
+//     for (int i = 0; i < result.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             Console.Write(result[i,j]);
+//         }
+
+//         Console.WriteLine();
+//     }
+// }
+// void ChangeRows(int [,] result)
+// {
+//     for (int j = 0; j < result.GetLength(1); j++)
+//     {
+//         int temp = result[0,j];
+//         result[0,j]=result[result.GetLength(0)-1,j];
+//         result[result.GetLength(0)-1,j]= temp;
+//     }
+// }
+
+// int [,] result = GetRandomArray(CreateArray(), 10);
+// PrintArray(result);
+// ChangeRows(result);
+// Console.WriteLine("________");
+// PrintArray(result);
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+//  В случае, если это невозможно,
+//  программа должна вывести сообщение для пользователя.
+int [,] CreateArray()
+{
+    Console.WriteLine("Введите колличество строк в массиве:");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов в массиве:");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    int [,] array =new  int [rows,columns];
+    return array;
+}
+int[,] GetRandomArray(int[,] array, int length)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      for (int j = 0; j < array.GetLength(1); j++)
+       {
+            array[i,j] =new Random().Next(length);
+        }
+    }
+    return array;
+}
+void ChangeArray(int [,] array1, int[,]array2)
+{
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+       {
+        array2[j,i]=array1[i,j];
+       }
+    }
+
+}
+void PrintArray(int[,]result)
+{
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            Console.Write(result[i,j]);
+        }
+
+        Console.WriteLine();
+    }
+}
+int [,] array1 = GetRandomArray(CreateArray(), 10);
+int[,] array2=CreateArray();
+PrintArray(array1);
+ChangeArray(array1, array2);
+PrintArray(array2);
+
+
+int minEl=int.MaxValue;
+int minRows=0;
+int minColumns=0;
+void DelList(int[,]result)
+{
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        if (i!=minRows)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+               if (j!=minColumns)
+               {
+                Console.Write(result[i,j]);
+               }
+              
+            }
+
+            Console.WriteLine();
+        }
+        
+    }
+}
+void GetArray(int [,]result)
+{
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = new Random().Next(0,10);
+
+            if (result[i,j]<minEl)
+            {
+                minEl=result[i,j];
+                minRows=i;
+                minColumns=j;
+            }
+        } 
+    }
+}
+
